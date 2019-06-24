56c1bc1178c7d4eef73255940b688a1832827eb8
Katie Humburg <pookakat@gmail.com>
2019-06-24 19:50:50 -0400

    set up theater with sliding curtains

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 console.log('Running');
 $('#modal-button').click(function(e){
     console.log('clicky');
-    $('#myModal').fadeOut();
-})
+    $('#myModal').fadeOut('slow', slideOpen());
+});
+
+function slideOpen(){
+    console.log("This is where the flexbox will slide open the doors");
+}
