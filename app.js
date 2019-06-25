a9f3e1b15088f9f61d75807f162daa6161d70fbf
Katie Humburg <pookakat@gmail.com>
2019-06-25 19:15:02 -0400

    try to get the other page to load

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 console.log('Running');
 $('#modal-button').click(function(e){
     console.log('clicky');
-    $('#myModal').fadeOut('slow', slideOpen());
+    $('#myModal').fadeOut(slideOpen());
 });
 
 function slideOpen(){
-    console.log("This is where the flexbox will slide open the doors");
+    $('#tada').load("https://jernical.github.io/Ding-su/");
     $('#stage').css({"justify-content": "space-between"});
-    $('#rtdoor').animate({'left':'100%'});
-    $('#ltdoor').animate({'right':'100%'});
+    $('#rtdoor').animate({'left':'100%'}, "slow");
+    $('#ltdoor').animate({'right':'100%'}, "slow");
 }
