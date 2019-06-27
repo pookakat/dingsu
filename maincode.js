81182c099e39061abc747b2cbb80725d5a19ccb6
Katie Humburg <pookakat@gmail.com>
2019-06-26 20:37:43 -0400

    make the menu pop up and work correctly

--- maincode.js
+++ maincode.js
@@ -3,3 +3,7 @@ $('body').fadeIn(1000);
 $(document).ready(function() {
     $('#title-text').animate({'margin-left': '0'}, 'slow');
 });
+
+$('#menu').click(function(){
+    $('#site-menu').fadeIn(300);
+})
