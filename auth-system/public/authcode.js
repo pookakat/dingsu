07335c1a5516567d911d57242c5b490a91f27a20
Katie Humburg <pookakat@gmail.com>
2019-06-29 11:04:31 -0400

    delete authentication crap

--- auth-system/public/authcode.js
+++
@@ -1,9 +0,0 @@
-$('#submit').on('click', function(event){
-    event.preventDefault();
-    console.log('submitting');
-    console.log($('#uName').val().trim(), $('#uPass').val().trim());
-    const data = {user_name: $('#uName').val().trim(), user_pass: $('#uPass').val().trim(), super_user: false};
-    console.log(data);
-    $.post('/user', data);
-    return false;
-})
--- auth-system/routes/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-    app.get('/user', function(req, res){
-        res.json({foo: 'bar'})
-    });
-    app.post('/user', function(req, res){
-        console.log(req.body);
-        res.json({foo: 'bar'})
-    });
-};
