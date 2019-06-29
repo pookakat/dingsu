d128b5544bc80cdc2b1297cf4a25c6c9e6285db6
Katie Humburg <pookakat@gmail.com>
2019-06-29 11:54:58 -0400

    employ the witchcraft of iframe

--- maincode.js
+++ maincode.js
@@ -1,8 +1,9 @@
+
 $('body').fadeIn(1000);
 
 $(document).ready(function() {
     $('#title-text').animate({'margin-left': '0'}, 'slow');
-    $('#main-site').load('http://google.com');
+    $('#current-data').attr('src', 'https://www.youtube.com/embed/NrJCDhf5N7k');
 });
 
 $('#menu').click(function(){
