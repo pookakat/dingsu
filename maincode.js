b529c70869863a4525a5850639cdc8f6546b147f
Katie Humburg <pookakat@gmail.com>
2019-06-29 11:09:28 -0400

    Update maincode.js

--- maincode.js
+++ maincode.js
@@ -2,7 +2,7 @@ $('body').fadeIn(1000);
 
 $(document).ready(function() {
     $('#title-text').animate({'margin-left': '0'}, 'slow');
-    $('#main-site').load('http://google.com');
+    $('#main-site').load('https://google.com');
 });
 
 $('#menu').click(function(){
